cd7f1a2e982483babf01b0a565dfe8c3ec0a5033
octavian-ilie <octavian.ilie@icloud.com>
2021-03-03 17:34:06 +0100

    Initial Commit

---
+++ TestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestApp
+//
+//  Created by Octavian Mihuț Ilie on 03/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("This is a test app.")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Octavian Mihuț Ilie on 03/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
